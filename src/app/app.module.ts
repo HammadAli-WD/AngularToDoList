273512a54caebe3c7cc70f7aee37a0ea89fef9dc
Hammad Ali <63840111+HammadAli-WD@users.noreply.github.com>
2021-01-24 23:32:03 +0100

    TS app Todo

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TodoItemComponent } from './components/todo-item/todo-item.component';
+import { TodosComponent } from './components/todos/todos.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TodoItemComponent,
+    TodosComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
