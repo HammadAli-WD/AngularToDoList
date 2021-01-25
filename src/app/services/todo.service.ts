1b4d82b81c6c6addc08e86fd4a0bc85c6fc33e5e
Hammad Ali <63840111+HammadAli-WD@users.noreply.github.com>
2021-01-25 04:25:30 +0100

    put request for toggling

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -2,6 +2,12 @@ import { Injectable } from '@angular/core';
 import { Observable } from "rxjs";
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Todo } from '../models/Todo';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json'
+  })
+}
 @Injectable({
   providedIn: 'root'
 })
@@ -11,11 +17,16 @@ export class TodoService {
 
   constructor(private http: HttpClient) { }
   
+  //Get Todos
   getTodos():Observable<Todo[]> {
     return this.http.get<Todo[]>
       (`${this.todosUrl}${this.todosLimit}`);
   }
 
-  //Delete Todo
+  //Toggle Completed
+  toggleCompleted(todo: Todo): Observable<any> {
+    const url = `${this.todosUrl}/${todo.id}`
+    return this.http.put(url, todo, httpOptions);
+  }
 
 }
