0992203a30be4882d02de531287090d7cf6a407d
Hammad Ali <63840111+HammadAli-WD@users.noreply.github.com>
2021-01-25 04:25:51 +0100

    delete

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -17,5 +17,7 @@ export class TodosComponent implements OnInit {
       this.todos = todos;
     }); 
   }
-
+  deleteTodo(todo: Todo) {
+    this.todos = this.todos.filter(t => t.id !== todo.id);
+  }
 }
