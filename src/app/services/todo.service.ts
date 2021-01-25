280e8f42f670ef547520271e33da11958f19368e
Hammad Ali <63840111+HammadAli-WD@users.noreply.github.com>
2021-01-25 03:27:43 +0100

    Revert "Revert "Addition of HttpClient Module""
    This reverts commit a6e9ecf28d415047009fe3ae521d87d167d66326.

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -1,28 +1,21 @@
 import { Injectable } from '@angular/core';
-
+import { Observable } from "rxjs";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Todo } from '../models/Todo';
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
+  todosUrl: String = 'https://jsonplaceholder.typicode.com/todos';
+  todosLimit = '?_limit=5';
 
-  constructor() { }
-  getTodos() {
-    return [
-      {
-        id: 1,
-        title: "Todo One",
-        completed: false
-      },
-      {
-        id: 2,
-        title: "Todo Two",
-        completed: true
-      },
-      {
-        id: 3,
-        title: "Todo Three",
-        completed: false
-      },
-    ]
+  constructor(private http: HttpClient) { }
+  
+  getTodos():Observable<Todo[]> {
+    return this.http.get<Todo[]>
+      (`${this.todosUrl}${this.todosLimit}`);
   }
+
+  //Delete Todo
+
 }
